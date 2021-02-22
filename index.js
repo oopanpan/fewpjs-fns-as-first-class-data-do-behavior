1ee7968920216fe39ff00758462ae147320084d6
oopanpan <oopanpan@gmail.com>
2021-02-22 15:47:54 -0800

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str){
+  const hr = str.split(':');
+  if (hr[0] < 12){
+    return 'Good Morning';
+  } else if (12 <= hr[0] && hr[0] < 17){
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(str){
+  document.getElementById('greeting').innerText = str
+}
